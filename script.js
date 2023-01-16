c995fdbf1206df7af8cd165a9b2f79ea82806d82
FarhanaRafi <118438210+FarhanaRafi@users.noreply.github.com>
2023-01-16 15:48:04 +0100

    implemented

---
+++ script.js
@@ -0,0 +1,74 @@
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "77cded920amsh41bb67a07527abep12c202jsn008eae99c17c",
+    "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+  },
+};
+
+let songs = [];
+
+const renderMusic = (searchResults, section) => {
+  console.log(section);
+  console.log(searchResults);
+  let sectionDiv = document.getElementById(section);
+  for (let i = 0; i < searchResults.data.length; i++) {
+    let result = searchResults.data[i];
+    songs.push(result);
+    sectionDiv.innerHTML += `<div class = "col">
+    <div class="card mb-3">
+    <img src="${result.album.cover_medium}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${result.title}</h5>
+    </div>
+    </div>
+  </div>`;
+  }
+};
+
+function convertSecs(seconds) {
+  let minutes = parseInt(seconds / 60);
+  let sec = seconds % 60;
+
+  return `${minutes}:${sec}`;
+}
+
+function songCount() {
+  let songCounts = document.getElementById("count");
+  songCounts.innerText = `(${songs.length})`;
+}
+
+function loadTracks() {
+  let rows = document.getElementById("tracks");
+  for (let i = 0; i < songs.length; i++) {
+    let song = songs[i];
+    rows.innerHTML += ` <tr>
+        <th scope="row">${i}</th>
+        <td>${song.title}</td>
+        <td>${song.album.title}</td>
+        <td>${song.artist.name}</td>
+        <td>${convertSecs(song.duration)}</td>
+      </tr>`;
+  }
+  songCount();
+}
+
+function getMusic(searchQuery, section) {
+  fetch(
+    "https://striveschool-api.herokuapp.com/api/deezer/search?q=" + searchQuery,
+    options
+  )
+    .then((response) => response.json())
+    .then((searchResults) => {
+      renderMusic(searchResults, section);
+    })
+    .catch((err) => console.error(err));
+}
+
+function loadSections() {
+  getMusic("pink floyd", "pink");
+  getMusic("metallica", "metal");
+  getMusic("daft punk", "daft");
+}
+
+window.onload = loadSections();
